5a1cb800e580de5ddd7f7f6a2171957a216ceb06
Evan <evansnyder16@gmail.com>
2022-04-14 14:13:30 -0400

    Added local unit test

--- week7/public/tests/recipe.spec.js
+++ week7/public/tests/recipe.spec.js
@@ -1,27 +1,28 @@
 const recipeService = new RecipeService();
 
-describe('Todo App', () => {
+describe('Recipe Browser App', () => {
   it('should initialize some HTML', () => {
-    spyOn(todo, 'init');
-    todo.init();
+    spyOn(recipeBrowser, 'init');
+    recipeBrowser.init();
 
-    expect(todo.init).toHaveBeenCalled();
+    expect(recipeBrowser.init).toHaveBeenCalled();
   });
 
   it('should add a recipe', async () => {
     const newRecipe = {
-      recipe_id: 0,
-      recipe_name: 'Third recipe',
-      status: 'pending',
-      created_date: '2020-04-14 22:50:32',
+      name: "TestRecipe",
+      grains: ["TestRecipe"],
+      hops: ["TestRecipe"],
+      yeast: "TestRecipe",
+      process: "TestRecipe"
     };
     const addRecipeServiceSpy = spyOn(recipeService, 'addRecipe');
 
-    expect(todo.recipes.length).toBe(0);
+    expect(recipeBrowser.recipes.length).toBe(0);
 
-    await todo.addRecipe(newRecipe);
+    await recipeBrowser.addRecipe(newRecipe);
 
     expect(addRecipeServiceSpy).toHaveBeenCalled();
-    expect(todo.recipes.length).toBe(1);
+    expect(recipeBrowser.recipes.length).toBe(1);
   });
 });
